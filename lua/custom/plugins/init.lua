a136d3d78f6449b579128fd4ad75a844a5a35db3
Andrea <siizx@live.it>
2025-04-16 14:43:37 +0200

    general update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -48,3 +48,33 @@ return {
     end,
   },
 }
+
+--  {
+--    'ThePrimeagen/harpoon',
+--    branch = 'harpoon2',
+--    dependencies = { 'nvim-lua/plenary.nvim' },
+--    config = function()
+--      local harpoon = require 'harpoon'
+--      harpoon:setup()
+--
+--      vim.keymap.set('n', '<leader>a', function()
+--        harpoon:list():add '$fzf'
+--      end, { desc = 'Harpoon add file' })
+--      vim.keymap.set('n', '<C-e>', function()
+--        harpoon.ui:toggle_quick_menu(harpoon:list())
+--      end, { desc = 'Harpoon menu' })
+--      vim.keymap.set('n', '<C-h>', function()
+--        harpoon:list():select(1)
+--      end)
+--      vim.keymap.set('n', '<C-t>', function()
+--        harpoon:list():select(2)
+--      end)
+--      vim.keymap.set('n', '<C-n>', function()
+--        harpoon:list():select(3)
+--      end)
+--      vim.keymap.set('n', '<C-s>', function()
+--        harpoon:list():select(4)
+--      end)
+--    end,
+--  },
+--}
