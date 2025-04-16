17ac9884f8fab9cb768cd6578beaf9b732cf8175
Andrea <siizx@live.it>
2025-04-16 09:03:49 +0200

    general update

---
+++ cancellare.lua
@@ -0,0 +1,8 @@
+ a a a
+   skaj
+   asdn
+   undo
+   6
+   7
+   8
+   9
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,49 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Undotree
+
+  {
+    'mbbill/undotree',
+    cmd = 'UndotreeToggle', -- 🔧 explicitly say this is a command we want to lazy-load on
+    keys = {
+      {
+        '<leader>u',
+        function()
+          vim.cmd 'UndotreeToggle'
+        end,
+        desc = 'Toggle Undotree',
+      },
+    },
+  },
+  -- Harpoon
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():append()
+      end, { desc = 'Harpoon add file' })
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon menu' })
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-t>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-n>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-s>', function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+}
